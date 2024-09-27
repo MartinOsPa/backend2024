c572e0600c2fa91cd25043311e7cc43856ca2b00
Martin Osorio Pascual <mo6312454@gmail.com>
2024-09-27 11:12:22 -0600

    Agrega el proyecto webserver

--- holaMundo/app.js
+++ holaMundo/app.js
@@ -1,3 +1,10 @@
-const { otromensaje } = require("./hola.js")
-console.log("Este es el punto de entrada")//mesaje de verificación de la app.js
-console.log(otromensaje)//mesaje de verificación de la app.js
+const { createServer } = require("https");
+
+const server = createServer((req, res) => {
+    res.write("Bienvenidos a mi primer servidor web");
+    res.end();
+});
+
+server.listen(8080);
+
+console.log("Servidor Web iniciado en 8080");
