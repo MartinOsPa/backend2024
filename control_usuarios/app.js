8deed5ea774959d4bfbffdd01f36c6a15e88af81
Martin Osorio Pascual <mo6312454@gmail.com>
2024-10-09 15:28:15 -0600

    Control_usuarios CON NUEVAS VALIDACIONES

--- control_usuarios/app.js
+++ control_usuarios/app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 
 const app = express();
+app.use(express.json());
+
 const usuarios = [
     {
         id: 1,
@@ -16,6 +18,7 @@ const usuarios = [
 
     }
 ];
+
 
 //GET para obetener información
 app.get("/usuarios", (req, res) => {
@@ -38,32 +41,55 @@ app.get("/usuarios", (req, res) => {
 
 });
 
-/*app.get("/usuarios/1", (req, res) => {
+
+app.get("/usuarios/1", (req, res) => {
     res.status(200).send(usuarios[0]);
+});
+app.get("/usuarios/1", (req, res) => {
+    res.status(200).send(usuarios[0]);
+
+});
+
+
+/*app.get("/usuarios/:id",(req, res)=>{
+    const {id}=req.params;
+    //const params = req.params;
+    //console.log(params);
+    //console.log(typeof(+id));
+    if(isNaN(id)){
+        res.status(400).send({error:"El id debe ser un numero"});
+        return;
+    };
+    const usuario= usuarios.find((usuario)=>usuario.id === +id);
+    res.status(200).send(usuario);
+    //res.status(200).send("probando");
+
 });*/
 
 app.get("/usuarios/:id", (req, res) => {
     const { id } = req.params;
-
-    if (isNaN(id)) {
-        res.status(400).send({ error: "El id debe ser numero" });
-        return;
-    };
-
-
-
-
+    //const params = req.params;
+    //console.log(params);
+    //console.log(typeof(+id));
     const usuario = usuarios.find((usuario) => usuario.id === +id);
-
     if (usuario === undefined) {
-        res.status(404).send({ error: "Usuario con ${id} no existe" });
+        res.status(404).send({ error: `el usuario con id ${id} no existe` });
         return;
     };
-    return;
-    //console.log(params)
-    // res.status(200).send(usuarios[0]);
     res.status(200).send(usuario);
-});
+    //res.status(200).send("probando");
+});//end point
+
+app.post("/usuarios", (req, res) => {
+    const { nombre, apellido, email } = req.body;
+    usuarios.push({ id: usuarios.length + 1, nombre, apellido, email })
+    res.status(201).send("El usuario se agrego correctamennte");
+    //console.log(req.body);
+    //res.send("Hola desde post!");
+}); // crear un nuevo recurso
+
+
+
 
 app.listen(3000, () => {
     console.log("Nuevo servidor corriendo en https://localhost:3000")
