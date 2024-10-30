6d5858e02ff523d582e8e3c3e7ca22e715a55bcc
MartinOsPa <mo6312454@gmail.com>
2024-10-30 11:47:58 -0600

    Agregar,actualizar,eliminar tarea

--- stock_manager/src/controllers/users.js
+++ stock_manager/src/controllers/users.js
@@ -26,9 +26,75 @@ const getById = (req = request, res = response) => {
     }
 
     res.send(user);
+
 }
 
 
+// Crear un nuevo usuario
+const createUser = (req = request, res = response) => {
+    const { name } = req.body;
+
+    if (!name) {
+        res.status(400).send('Name is required');
+        return;
+    }
+    const user = users.find(user => user.name === name);
+
+    if (user) {
+        res.status(409).send('User alredy exists');
+        return;
+    }
+
+    users.push({ id: users.length + 1, name });
+    res.send('User created succesfully');
+};
+
+// Actualizar un usuario
+const updateUser = (req = request, res = response) => {
+    const { id } = req.params;
+    const { name } = req.body;
+
+    if (isNaN(id)) {
+        res.status(400).send('Invalid ID');
+        return;
+    }
+
+    const user = users.find(user => user.id === +id);
+
+    if (!user) {
+        res.status(404).send('User not found');
+        return;
+    }
+
+    users.forEach(user => {
+        if (user.id === +id) {
+            user.name = name;
+        }
+    });
+    res.send('user update succerfully');
+}
+
+// Eliminar un usuario por ID
+const deleteUser = (req = request, res = response) => {
+    const { id } = req.params;
+
+    if (isNaN(id)) {
+        res.status(400).send('Invalid ID');
+        return;
+    }
+
+    const user = users.find(user => user.id === +id);
+
+    if (!user) {
+        res.status(404).send('User not found');
+        return;
+    }
+
+    users.splice(users.findIndex((user) => user.id === +id), 1);
+    res.send('User deleted');
+};
+
+module.exports = { getAll, getById, createUser, updateUser, deleteUser };
 
 /*const getMessage = (req = request, res = response) => {
     res.send('Helllo from the users conrtoller!');
@@ -38,4 +104,3 @@ const getById = (req = request, res = response) => {
 module.exports = { getMessage};
 */
 
-module.exports = { getAll, getById };
